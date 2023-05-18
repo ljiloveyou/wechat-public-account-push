e1f5c60b2f8157e953a786e637bb7f433e422b5f
ljiloveyou <133562422+ljiloveyou@users.noreply.github.com>
2023-05-19 00:26:09 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -1,67 +1,87 @@
-/* eslint-disable */
-
-/**
- * 此项目配置为方便新人使用，已缩减至最简配置。
- * 如若想使用更多功能，请查考文档中的 【3. config参数说明】 
- * 自行添加属性，以支持更多个性化功能
- */
 const USER_CONFIG = {
+  USE_PASSAGE: 'wechat-test',
+  
 
-  // 使用微信测试号：公众号APP_ID
-  APP_ID: '',
+  MAX_PUSH_ONE_MINUTE: 5,
+  SLEEP_TIME: 65000,
 
-  // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: '',
+  SWITCH: {
+    /** 每日天气 */
+    weather: true,
+    /** 节假日 */
+    holidaytts: true,
+    /** 每日N句 */
+    CIBA: true,
+    // 每日一言
+    oneTalk: true,
+    
+    // 土味情话(彩虹屁)
+    earthyLoveWords: true,
+    
+    // 朋友圈文案, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    momentCopyrighting: false,
+    
+    // 毒鸡汤, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    poisonChickenSoup: false,
+    
+    // 古诗古文, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    poetry: true,
 
-  PROVINCE: '广东',
-  CITY: '惠州',
+    /** 星座运势 */
+    // 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    horoscope: true,
+  
+    /** 生日消息和节日消息 */
+    // 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    birthdayMessage: true,
+  
+    /** 学生课表 */
+    // 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    courseSchedule: false,
+  },
+  IS_SHOW_COLOR: false,
 
   USERS: [
     {
-      // 想要发送的人的名字
-      name: '宝贝',
-      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
-      // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
-      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
+      name: '宝宝',
+
+      id: 'okUcl563-6MsvWP3ZmyRgzKqiHNg',
+
+      useTemplateId: 'FX4c2ETj9lCsg1f12e_hUgiQpRmPk9MjOVBwsmt4qw4',
+
+      province: '甘肃省',
+
+      city: '兰州市',
+      // 新历生日, 仅用作获取星座运势, 格式必须
+      horoscopeDate: '02-11',
+      // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
+      horoscopeDateType: '今日',
+
+      openUrl: 'www.baidu.com',
+      // 专属节日提醒，如果你在这里填写了节日提醒，就不会执行FESTIVALS的提醒了, 和FESTIVALS的配置方法相同，可以往下查看，我这里就不重复写了
       festivals: [
-        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号,没有*是阳历
         {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
-        },
-        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-        {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
-        },
-        {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
-        },
+          type: '生日', name: '宝宝', year: '2000', date: '02-11',
+        }
       ],
-      // 我们在一起已经有xxxx天了的配置
+      // 专属纪念日/倒数日，如果你在这里填写了纪念日/倒数日，就不会计算CUSTOMIZED_DATE_LIST的日子了, 和CUSTOMIZED_DATE_LIST的配置方法相同，可以往下查看，我这里就不重复写了
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
-        // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
+        { keyword: 'love_day', date: '2023-02-24' }
       ],
-    },
+      },
   ],
 
-
-  // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: 'uZbwYMFxbbBn8EWU8ZkIcSOGheLtlnsenZdBpPo6nss',
 
   CALLBACK_USERS: [
     {
+
       name: '自己',
-      // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+
+      id: 'okUcl55AQhkNaxoxbBM4EDVHzR0o',
     }
   ],
-
-}
-
+  }
 module.exports = USER_CONFIG
-
